5f7129b5515bc1c85414d4c6ad65118b4153469e
Alina Mironova <tommilin-07.13@yandex.ru>
2021-05-19 21:15:26 +0300

    Props

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+const todoItem=['Изучить новый материал','Выполнить ДЗ','Прочить книгу'];
 const App = () => (<div>
    <h1> Важные дела </h1>
    <InputItem />
-   <ItemList />
-   <Footer />
+   <ItemList todoItem={todoItem} />
+   <Footer count={2} />
 </div>
 );
 export default App;
