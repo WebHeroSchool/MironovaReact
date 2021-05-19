66f6a866bc65d403c58194fb1a95ad4a3a50da4e
Alina Mironova <tommilin-07.13@yandex.ru>
2021-05-19 21:49:38 +0300

    Вывод списка элементов

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
-const todoItem=['Изучить новый материал','Выполнить ДЗ','Прочить книгу'];
-const App = () => (<div>
+// const todoItem=['Изучить новый материал','Выполнить ДЗ','Прочить книгу'];
+const App = () => {
+  const items =[
+    {
+        value: 'Написать новое приложение'
+    },
+    {
+        value: 'прописать props'
+     },
+    {
+        value: 'сделать все дела'
+    }
+  ];
+  return (
+  <div>
    <h1> Важные дела </h1>
    <InputItem />
-   <ItemList todoItem={todoItem} />
+   <ItemList items={items} />
    <Footer count={2} />
-</div>
-);
+</div>);
+}
 export default App;
