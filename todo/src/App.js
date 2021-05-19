c4fb54212ba3db334045847aed8049eebe00d226
Alina Mironova <tommilin-07.13@yandex.ru>
2021-05-19 19:41:08 +0300

    react компоненты

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,10 @@
-const element = (
-  <div>
-  Hello!
-    <a href="">React</a>
-  </div>
-);
-export { element };
+const ItemList=()=>(<ul>
+<li>1</li>
+<li>2</li>
+<li>3</li>
+</ul>);
+const App = ()=> (<div>
+  <h1>Hello!</h1>
+  <ItemList />
+  </div>);
+export default App;
