55e5d8dcc4f0d1670cbef4bcec53af5317101181
Alina Mironova <tommilin-07.13@yandex.ru>
2021-06-26 23:02:59 +0300

    Изм

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import FormHelperText from "@material-ui/core/FormHelperText";
 import styles from './inputItem.module.css';
 class InputItem extends React.Component{
   state={
