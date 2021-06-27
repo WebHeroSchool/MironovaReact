42d168e9c32d4a38799fb5f979aabcf965b162ac
Alina Mironova <tommilin-07.13@yandex.ru>
2021-06-27 09:20:48 +0300

    Утечка памяти

--- src/components/Item/Items.js
+++ src/components/Item/Items.js
@@ -10,13 +10,10 @@ import classnames from 'classnames';
 import styles from './item.module.css';
 class Item extends React.Component{
   componentDidMount(){
-    console.log('componentDidMount');
-  }
-  componentDidUpdate(){
-    console.log('componentDidUpdate');
+    this.timerID = setInterval(() => console.log('interval'), 1000)
   }
   componentWillUnmount(){
-    console.log('componentWillUnmount');
+    clearInterval(this.timerID);
   }
   render(){
     const { value, isDone, onClickDone,onClickDelete, id }=this.props;
